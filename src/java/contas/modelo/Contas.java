c85daffcfaf0c095d6d0481cab963ffce4a8e874
comp2 <comp2@comp2_09.lab.unesc.net>
2016-11-16 20:02:50 -0200

    Realizado alterações referentes a template

---
+++ src/java/contas/modelo/Contas.java
@@ -0,0 +1,70 @@
+
+package contas.modelo;
+
+import java.util.Date;
+
+public class Contas {
+    
+    int idconta,idusuario;
+    String descricao;
+    double valor;
+    Date data_pagamento,data_vencimento;
+    int situacao; // 1 = á vencer 2= Pago 3 = vencido 
+
+    public int getIdconta() {
+        return idconta;
+    }
+
+    public void setIdconta(int idconta) {
+        this.idconta = idconta;
+    }
+
+    public int getIdusuario() {
+        return idusuario;
+    }
+
+    public void setIdusuario(int idusuario) {
+        this.idusuario = idusuario;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public Date getData_pagamento() {
+        return data_pagamento;
+    }
+
+    public void setData_pagamento(Date data_pagamento) {
+        this.data_pagamento = data_pagamento;
+    }
+
+    public Date getData_vencimento() {
+        return data_vencimento;
+    }
+
+    public void setData_vencimento(Date data_vencimento) {
+        this.data_vencimento = data_vencimento;
+    }
+
+    public int getSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(int situacao) {
+        this.situacao = situacao;
+    }
+
+}
---
+++ src/java/contas/modelo/Usuario.java
@@ -0,0 +1,33 @@
+
+package contas.modelo;
+
+public class Usuario {
+    
+    int idusuario;
+    String nome;
+    double saldo;
+
+    public int getIdusuario() {
+        return idusuario;
+    }
+
+    public void setIdusuario(int idusuario) {
+        this.idusuario = idusuario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+}
